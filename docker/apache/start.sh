3c3e71e783b91f2862a3eca702379968b80e6704
ssachtleben <ssachtleben@gmail.com>
2018-03-15 17:19:37 +0100

    fixed ownership

--- docker/apache/start.sh
+++ docker/apache/start.sh
@@ -7,6 +7,7 @@ sleep 30
 rm -fR /var/www/shopware
 mkdir /var/www/shopware
 sw install:release --release="5.4.0" --install-dir="/var/www/shopware" --shop-host="localhost" --shop-path="/" --db-host="shopware5-mysql" --db-user="app" --db-password="app" --db-name="shopware5" --admin-username="styla" --admin-password="support" --admin-name="Styla Admin" --admin-email="support@styla.com"
+chown -R www-data:www-data /var/www/shopware
 
 # Symlink plugin
 # /var/www/shopware/engine/Shopware/Plugins/Local/Frontend/StylaSEO
