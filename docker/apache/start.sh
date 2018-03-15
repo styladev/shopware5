81367b2c9d3a7301069ea12a9a7b6b466ad8e9d9
ssachtleben <ssachtleben@gmail.com>
2018-03-15 17:09:04 +0100

    added sleep

--- docker/apache/start.sh
+++ docker/apache/start.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Sleep for 5 seconds to wait until mysql is up
+sleep 5
+
 # Install Shopware
-rm -fR /var/www/shopware/
+rm -fR /var/www/shopware
+mkdir /var/www/shopware
 sw install:release --release="5.4.0" --install-dir="/var/www/shopware" --shop-host="localhost" --shop-path="/" --db-host="shopware5-mysql" --db-user="app" --db-password="app" --db-name="shopware5" --admin-username="styla" --admin-password="support" --admin-name="Styla Admin" --admin-email="support@styla.com"
 
 # Symlink plugin
