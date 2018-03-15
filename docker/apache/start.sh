cd3a9bb3260f0df5b7c5f48ad56ea9742b1f41ff
Antonio Cosentino <hello@antoniocosentino.it>
2018-03-15 17:57:38 +0100

    removing shop path parameter to fix linking issues

--- docker/apache/start.sh
+++ docker/apache/start.sh
@@ -6,7 +6,7 @@ sleep 30
 # Install Shopware
 rm -fR /var/www/shopware
 mkdir /var/www/shopware
-sw install:release --release="5.4.0" --install-dir="/var/www/shopware" --shop-host="localhost" --shop-path="/" --db-host="shopware5-mysql" --db-user="app" --db-password="app" --db-name="shopware5" --admin-username="styla" --admin-password="support" --admin-name="Styla Admin" --admin-email="support@styla.com"
+sw install:release --release="5.4.0" --install-dir="/var/www/shopware" --shop-host="localhost" --db-host="shopware5-mysql" --db-user="app" --db-password="app" --db-name="shopware5" --admin-username="styla" --admin-password="support" --admin-name="Styla Admin" --admin-email="support@styla.com"
 chown -R www-data:www-data /var/www/shopware
 
 # TODO: Symlink plugin
