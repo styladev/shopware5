948e33cdf877c13f10dfa24b8678e5dacdfbe3b4
ssachtleben <ssachtleben@gmail.com>
2018-03-15 17:03:08 +0100

    delete folder before installing again

--- docker/apache/start.sh
+++ docker/apache/start.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 # Install Shopware
-cd "$(dirname /var/www/shopware)"
-sw install:release --release="5.4.0" --install-dir="." --shop-host="localhost" --shop-path="/" --db-host="shopware5-mysql" --db-user="app" --db-password="app" --db-name="shopware5" --admin-username="styla" --admin-password="support" --admin-name="Styla Admin" --admin-email="support@styla.com"
+rm -fR /var/www/shopware/
+sw install:release --release="5.4.0" --install-dir="/var/www/shopware" --shop-host="localhost" --shop-path="/" --db-host="shopware5-mysql" --db-user="app" --db-password="app" --db-name="shopware5" --admin-username="styla" --admin-password="support" --admin-name="Styla Admin" --admin-email="support@styla.com"
+
+# Symlink plugin
+# /var/www/shopware/engine/Shopware/Plugins/Local/Frontend/StylaSEO
 
 # Starting apache
 apache2-foreground
