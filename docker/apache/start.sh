39e103c3d01e3e6122ca2bb2671401ac6c5e2bca
ssachtleben <ssachtleben@gmail.com>
2018-03-15 17:26:47 +0100

    added todos

--- docker/apache/start.sh
+++ docker/apache/start.sh
@@ -6,11 +6,17 @@ sleep 30
 # Install Shopware
 rm -fR /var/www/shopware
 mkdir /var/www/shopware
-sw install:release --release="5.4.0" --install-dir="/var/www/shopware" --shop-host="localhost" --shop-path="/" --db-host="shopware5-mysql" --db-user="app" --db-password="app" --db-name="shopware5" --admin-username="styla" --admin-password="support" --admin-name="Styla Admin" --admin-email="support@styla.com"
+sw install:release --release="5.4.0" --install-dir="/var/www/shopware" --shop-host="localhost" --shop-path="" --db-host="shopware5-mysql" --db-user="app" --db-password="app" --db-name="shopware5" --admin-username="styla" --admin-password="support" --admin-name="Styla Admin" --admin-email="support@styla.com"
 chown -R www-data:www-data /var/www/shopware
 
-# Symlink plugin
-# /var/www/shopware/engine/Shopware/Plugins/Local/Frontend/StylaSEO
+# TODO: Symlink plugin
+#ln -s /home/root/StylaSEO /var/www/shopware/engine/Shopware/Plugins/Local/Frontend/StylaSEO
+
+# TODO: install demo data:
+#/var/www/shopware/bin/console sw:plugin:install SwagDemoDataEN
+
+# TODO: configurate plugin settings e.g.:
+#/var/www/shopware/bin/console sw:plugin:config:set StylaSEO styla_username {{ styla_username }}
 
 # Starting apache
 apache2-foreground
