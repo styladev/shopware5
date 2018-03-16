e06447271ded2a755c368d102bf01ac8587e47e1
ssachtleben <ssachtleben@gmail.com>
2018-03-16 09:58:59 +0100

    improved plugin configuration

--- docker/apache/start.sh
+++ docker/apache/start.sh
@@ -9,12 +9,15 @@ mkdir /var/www/shopware
 sw install:release --release="5.4.0" --install-dir="/var/www/shopware" --shop-host="localhost" --db-host="shopware5-mysql" --db-user="app" --db-password="app" --db-name="shopware5" --admin-username="styla" --admin-password="support" --admin-name="Styla Admin" --admin-email="support@styla.com"
 chown -R www-data:www-data /var/www/shopware
 
+# Copy plugin
+ln -s /home/root/StylaSEO /var/www/shopware/engine/Shopware/Plugins/Local/Frontend/StylaSEO
+
 # Configurare Shopware
 /var/www/shopware/bin/console sw:firstrunwizard:disable
+/var/www/shopware/bin/console sw:plugin:refresh
 #/var/www/shopware/bin/console sw:plugin:install SwagDemoDataEN # TODO: Plugin by name "SwagDemoDataEN" was not found ?!?
 
 # Configurate plugin
-ln -s /home/root/StylaSEO /var/www/shopware/engine/Shopware/Plugins/Local/Frontend/StylaSEO
 /var/www/shopware/bin/console sw:plugin:install StylaSEO
 /var/www/shopware/bin/console sw:plugin:activate StylaSEO
 /var/www/shopware/bin/console sw:plugin:config:set StylaSEO styla_username "ci-shopware5"
@@ -23,6 +26,9 @@ ln -s /home/root/StylaSEO /var/www/shopware/engine/Shopware/Plugins/Local/Fronte
 /var/www/shopware/bin/console sw:plugin:config:set StylaSEO styla_seo_url "http://seoapi.stage.eu.magalog.net"
 /var/www/shopware/bin/console sw:plugin:config:set StylaSEO styla_modular_content_username "ci-shopware5-pd"
 /var/www/shopware/bin/console sw:plugin:config:set StylaSEO styla_modular_content_api "http://frontend-gateway.stage.eu.magalog.net"
+
+# Finalize
+/var/www/shopware/bin/console sw:cache:clear
 
 # Starting apache
 apache2-foreground
