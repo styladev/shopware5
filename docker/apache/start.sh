0aa5df7fd2875bb370af29d38ce48398f973733f
ssachtleben <ssachtleben@gmail.com>
2018-03-16 09:49:05 +0100

    added plugin configuration

--- docker/apache/start.sh
+++ docker/apache/start.sh
@@ -9,14 +9,20 @@ mkdir /var/www/shopware
 sw install:release --release="5.4.0" --install-dir="/var/www/shopware" --shop-host="localhost" --db-host="shopware5-mysql" --db-user="app" --db-password="app" --db-name="shopware5" --admin-username="styla" --admin-password="support" --admin-name="Styla Admin" --admin-email="support@styla.com"
 chown -R www-data:www-data /var/www/shopware
 
-# TODO: Symlink plugin
-#ln -s /home/root/StylaSEO /var/www/shopware/engine/Shopware/Plugins/Local/Frontend/StylaSEO
+# Configurare Shopware
+/var/www/shopware/bin/console sw:firstrunwizard:disable
+#/var/www/shopware/bin/console sw:plugin:install SwagDemoDataEN # TODO: Plugin by name "SwagDemoDataEN" was not found ?!?
 
-# TODO: install demo data:
-#/var/www/shopware/bin/console sw:plugin:install SwagDemoDataEN
-
-# TODO: configurate plugin settings e.g.:
-#/var/www/shopware/bin/console sw:plugin:config:set StylaSEO styla_username {{ styla_username }}
+# Configurate plugin
+ln -s /home/root/StylaSEO /var/www/shopware/engine/Shopware/Plugins/Local/Frontend/StylaSEO
+/var/www/shopware/bin/console sw:plugin:install StylaSEO
+/var/www/shopware/bin/console sw:plugin:activate StylaSEO
+/var/www/shopware/bin/console sw:plugin:config:set StylaSEO styla_username "ci-shopware5"
+/var/www/shopware/bin/console sw:plugin:config:set StylaSEO styla_basedir "magazine"
+/var/www/shopware/bin/console sw:plugin:config:set StylaSEO styla_api_url "https://client-scripts.stage.eu.magalog.net"
+/var/www/shopware/bin/console sw:plugin:config:set StylaSEO styla_seo_url "http://seoapi.stage.eu.magalog.net"
+/var/www/shopware/bin/console sw:plugin:config:set StylaSEO styla_modular_content_username "ci-shopware5-pd"
+/var/www/shopware/bin/console sw:plugin:config:set StylaSEO styla_modular_content_api "http://frontend-gateway.stage.eu.magalog.net"
 
 # Starting apache
 apache2-foreground
