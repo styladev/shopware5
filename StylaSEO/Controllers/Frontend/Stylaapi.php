61906841235b27b369ef6ace9e1aed9b2383461e
Kenan Sulayman <kenan@sly.mn>
2019-04-23 15:43:25 +0200

    StylaSEO: use EK prices; general cleanup

--- StylaSEO/Controllers/Frontend/Stylaapi.php
+++ StylaSEO/Controllers/Frontend/Stylaapi.php
@@ -5,9 +5,6 @@ class Shopware_Controllers_Frontend_Stylaapi extends Shopware_Controllers_Fronte
 
     public function indexAction()
     {
-        //$this->View()->setScope(Enlight_Template_Manager::SCOPE_PARENT);
-        //$this->View()->extendsTemplate('frontend/custom/index.tpl');
-        //$this->View()->loadTemplate('frontend/stylaapi/index.tpl');
         $this->View()->assign('someNumber', 5);
     }
 
@@ -100,7 +97,6 @@ class Shopware_Controllers_Frontend_Stylaapi extends Shopware_Controllers_Fronte
 
             $filterArticles = Shopware()->Db()->fetchAll($sql);
             $filter['id'] = array_column($filterArticles, 'id');
-            //echo '<pre>'; print_r($filter['id']); exit;
 
         } else if ($categoryId > 0) {
             $sql = "SELECT a.id, a.name
@@ -131,11 +127,8 @@ class Shopware_Controllers_Frontend_Stylaapi extends Shopware_Controllers_Fronte
 
             $searchArticles = Shopware()->Db()->fetchAll($sql);
             $filter['id'] = array_column($searchArticles, 'id');
-            //echo '<pre>'; print_r($filter['id']); exit;
         }
 
-
-        //echo '<pre>'; print_r($filter); exit;
         $result = $resource->getList($offset, $limit, $filter, $sort, array(
             'language' => $this->Request()->getParam('language')
         ));
@@ -169,24 +162,25 @@ class Shopware_Controllers_Frontend_Stylaapi extends Shopware_Controllers_Fronte
 
                 if ($articleDetails['sConfigurator']){
                     foreach ($articleDetails['sConfigurator'] as $variant) {
-                        if ($variant['values']) {
-                            foreach ($variant['values'] as $singleValue) {
-                                if ($singleValue['media']['source']) {
-                                    if (!in_array($singleValue['media']['source'], $imagesNewArr)){
-                                        $imagesNewArr[] = $singleValue['media']['source'];
-                                    }
-                                }
+                        if (!$variant['values']) {
+                            continue;
+                        }
+
+                        foreach ($variant['values'] as $singleValue) {
+                            if (!$singleValue['media']['source']) {
+                                continue;
                             }
+
+                            if (in_array($singleValue['media']['source'], $imagesNewArr)){
+                                continue;
+                            }
+
+                            $imagesNewArr[] = $singleValue['media']['source'];
                         }
                     }
                 }
 
                 switch ($imagesMethod) {
-                    case 'v1': //old method for images (default)
-                    default:
-                        $defImages = $imagesArr;
-                        break;
-
                     case 'v2': //new method for images
                         $defImages = $imagesNewArr;
                         break;
@@ -195,6 +189,10 @@ class Shopware_Controllers_Frontend_Stylaapi extends Shopware_Controllers_Fronte
                         $defImages = array_values(array_unique(array_merge($imagesArr, $imagesNewArr)));
                         break;
 
+                    case 'v1': //old method for images (default)
+                    default:
+                        $defImages = $imagesArr;
+                        break;
                 }
 
                 $res[] = array(
@@ -208,7 +206,7 @@ class Shopware_Controllers_Frontend_Stylaapi extends Shopware_Controllers_Fronte
                 error_log("Failed to retrieve article information for id " . $value['id'] . " : " . $e->getMessage());
             }
         }
-        //echo '<pre>'; print_r($res); exit;
+
         $res = json_encode($res, JSON_PRETTY_PRINT);
         echo $res;
         exit;
@@ -222,19 +220,18 @@ class Shopware_Controllers_Frontend_Stylaapi extends Shopware_Controllers_Fronte
         $baseFile = $config->get('baseFile');
 
         $detail = $baseFile . "?sViewport=detail&sArticle=" . $productId;
+
         if ($categoryId) {
             $detail .= '&sCategory=' . $categoryId;
         }
 
-        $rewrite = Shopware()->Modules()->Core()->sRewriteLink($detail, $productName);
-        return $rewrite;
+        return Shopware()->Modules()->Core()->sRewriteLink($detail, $productName);
     }
 
     public function getCurrency()
     {
         $shop = Shopware()->Shop();
-        $currency = $shop->getCurrency()->toArray();
-        return $currency;
+        return $shop->getCurrency()->toArray();
     }
 
     public function throwErr($error)
@@ -243,41 +240,84 @@ class Shopware_Controllers_Frontend_Stylaapi extends Shopware_Controllers_Fronte
         $message['saleable'] = false;
 
         echo json_encode($message);
+
         exit;
+    }
+
+    public function getEKPrices($detail) {
+        $productEKPrice = null;
+        $productEKPseudoPrice = null;
+
+        foreach ( $detail['prices'] as $price ) {
+            if ($price['customerGroupKey'] !== 'EK') {
+                continue;
+            }
+
+            if (
+                !array_key_exists('customerGroup', $price)
+                || $price['customerGroup']['key'] !== 'EK'
+            ) {
+                continue;
+            }
+
+            $productEKPrice = $price['price'];
+            $productEKPseudoPrice = $price['pseudoPrice'];
+        }
+
+        return [
+            "price" => $productEKPrice,
+            "pseudoPrice" => $productEKPseudoPrice,
+        ];
     }
 
     public function productAction()
     {
         $resource = \Shopware\Components\Api\Manager::getResource('article');
+
         $currencyInfo = $this->getCurrency();
+
         $id = $this->Request()->getParam('id');
         $useNumberAsId = (boolean)$this->Request()->getParam('useNumberAsId', 0);
 
+        $article = [];
+
         if ($useNumberAsId) {
             try {
-                $article = $resource->getOneByNumber($id, array(
+                $article = $resource->getOneByNumber($id, [
                     'language' => $this->Request()->getParam('language'),
-                    'considerTaxInput' => $this->Request()->getParam('considerTaxInput'),
-                ));
+                    'considerTaxInput' => $this->Request()->getParam('considerTaxInput')
+                ]);
             } catch (Exception $e) {
                 $this->throwErr($e->getMessage());
             }
         } else {
             try {
-                $article = $resource->getOne($id, array(
+                $article = $resource->getOne($id, [
                     'language' => $this->Request()->getParam('language'),
                     'considerTaxInput' => $this->Request()->getParam('considerTaxInput')
-                ));
+                ]);
             } catch (Exception $e) {
                 $this->throwErr($e->getMessage());
             }
         }
 
-        $taxInclPrice = $article['mainDetail']['prices'][0]['price'] + ($article['mainDetail']['prices'][0]['price'] * ($article['tax']['tax'] / 100));
+        $ekPrices = $this->getEKPrices($article['mainDetail']);
+        $productEKPrice = $ekPrices['price'];
+        $productEKPseudoPrice = $ekPrices['pseudoPrice'];
+
+        /*
+            This point will be undefined behavior if no EK price exists.
+        */
+
+        $taxRate = $article['tax']['tax'] / 100;
+
+        $taxInclPrice = $productEKPrice * ($taxRate + 1);
+
         $priceFormatted = money_format("%.2n", $taxInclPrice);
-        $oldPrice = '';
-        if ($article['mainDetail']['prices'][0]['pseudoPrice'] > 0) {
-            $taxInclOldPrice = $article['mainDetail']['prices'][0]['pseudoPrice'] + ($article['mainDetail']['prices'][0]['pseudoPrice'] * ($article['tax']['tax'] / 100));
+        $oldPriceFormatted = '';
+
+        if ($productEKPseudoPrice > 0) {
+            $taxInclOldPrice = $productEKPseudoPrice * ($taxRate + 1);
             $oldPriceFormatted = money_format("%.2n", $taxInclOldPrice);
         }
 
@@ -292,20 +332,32 @@ class Shopware_Controllers_Frontend_Stylaapi extends Shopware_Controllers_Fronte
                 break;
         }
 
-        $res = array('id' => $article['mainDetail']['number'], //$article['id']
+        $isActive = $article['active'];
+        $hasStock = $article['mainDetail']['inStock'] > 0;
+
+        $isLastStock = $article['lastStock'];
+
+        $minPurchase = $article['mainDetail']['minPurchase'];
+        $maxPurchase = $article['mainDetail']['maxPurchase'];
+
+        $res = [
+            'id' => $article['mainDetail']['number'],
             'name' => htmlentities($article['name']),
             'description' => $article['description'],
             'categories' => array_column($article['categories'], 'id'),
-            'saleable' => ($article['active'] && ($article['mainDetail']['inStock'] > 0 || !$article['lastStock'])) ? 'true' : 'false',
+            'saleable' => ($isActive && ($hasStock || !$isLastStock)) ? 'true' : 'false',
             'price' => $priceFormatted,
             'priceTemplate' => $priceTemplate,
             'oldPrice' => $oldPriceFormatted,
-            'minqty' => $article['mainDetail']['minPurchase'],
-            'maxqty' => ($article['mainDetail']['maxPurchase'] > 0) ? $article['mainDetail']['maxPurchase'] : 100,
-            'tax' => array(
-                'rate' => $article['tax']['tax'], 'label' => $article['tax']['name'], 'taxIncluded' => 'true', 'showLabel' => 'true'
-            ),
-        );
+            'minqty' => $minPurchase,
+            'maxqty' => ($maxPurchase > 0) ? $maxPurchase : 100,
+            'tax' => [
+                'rate' => $article['tax']['tax'],
+                'label' => $article['tax']['name'],
+                'taxIncluded' => 'true',
+                'showLabel' => 'true'
+            ],
+        ];
 
         foreach ($article['configuratorSet']['groups'] as $variant) {
             $res['attributes'][$variant['id']] = array(
@@ -314,34 +366,74 @@ class Shopware_Controllers_Frontend_Stylaapi extends Shopware_Controllers_Fronte
             );
 
             foreach ($article['mainDetail']['configuratorOptions'] as $m_option) {
-
-                if ($variant['id'] == $m_option['groupId']) {
-                    //$rr = (array)$res['attributes'][ $variant['id'] ]['options'][ $m_option['id'] ]['products'];
-                    $saleable = $article['mainDetail']['active'] && ($article['mainDetail']['inStock'] > 0 || !$article['lastStock']) ? 'true' : 'false';
-                    $res['attributes'][$variant['id']]['options'][$m_option['id']] = array(
-                        'id' => $m_option['id'],
-                        'label' => htmlentities($m_option['name']),
-                        'price' => $priceFormatted,
-                        //'products' => array_merge($rr, array($article['mainDetail']['number']))
-                        'products' => array(0 => array('id' => $article['mainDetail']['number'], 'saleable' => $saleable))
-                    );
+                if ($variant['id'] != $m_option['groupId']) {
+                    continue;
                 }
+
+                $isActive = $article['mainDetail']['active'];
+                $hasStock = $article['mainDetail']['inStock'] > 0;
+
+                $isLastStock = $article['lastStock'];
+
+                $saleable = $isActive && ($hasStock || !$isLastStock) ? 'true' : 'false';
+
+                $res['attributes'][$variant['id']]['options'][$m_option['id']] = array(
+                    'id' => $m_option['id'],
+                    'label' => htmlentities($m_option['name']),
+                    'price' => $priceFormatted,
+                    'products' => [
+                        0 => [
+                            'id' => $article['mainDetail']['number'],
+                            'saleable' => $saleable
+                        ]
+                    ]
+                );
             }
+
             unset($m_option);
 
             foreach ($article['details'] as $configuration) {
                 foreach ($configuration['configuratorOptions'] as $m_option) {
-                    if ($variant['id'] == $m_option['groupId']) {
-                        $rrr = (array)$res['attributes'][$variant['id']]['options'][$m_option['id']]['products'];
-                        $saleable = $configuration['active'] && ($configuration['inStock'] > 0 || !$article['lastStock']) ? 'true' : 'false';
-                        $res['attributes'][$variant['id']]['options'][$m_option['id']] = array(
-                            'id' => $m_option['id'],
-                            'label' => htmlentities($m_option['name']),
-                            'price' => (string)number_format(($configuration['prices'][0]['price'] * ($article['tax']['tax'] / 100 + 1)), 2, '.', ''),
-                            //'products' => array_merge($rrr, array($configuration['number']))
-                            'products' => array_merge($rrr, array(0 => array('id' => $configuration['number'], 'saleable' => $saleable, 'price' => (string)number_format(($configuration['prices'][0]['price'] * ($article['tax']['tax'] / 100 + 1)), 2, '.', ''))))
-                        );
+                    if ($variant['id'] != $m_option['groupId']) {
+                        continue;
                     }
+
+                    $isActive = $configuration['active'];
+                    $hasStock = $configuration['inStock'] > 0;
+
+                    $isLastStock = $article['lastStock'];
+
+                    $saleable = $isActive && ($hasStock || !$isLastStock) ? 'true' : 'false';
+
+                    $variantId = $variant['id'];
+                    $optionId = $m_option['id'];
+
+                    $variantOptionProducts = (array) $res['attributes'][$variantId]['options'][$optionId]['products'];
+
+                    $ekPrices = $this->getEKPrices($configuration);
+
+                    $formattedOptionPrice = (string)number_format(
+                        $ekPrices['price'] * ($taxRate + 1),
+                        2,
+                        '.',
+                        ''
+                    );
+
+                    $res['attributes'][$variant['id']]['options'][$m_option['id']] = array(
+                        'id' => $m_option['id'],
+                        'label' => htmlentities($m_option['name']),
+                        'price' => $formattedOptionPrice,
+                        'products' => array_merge(
+                            $variantOptionProducts,
+                            [
+                                0 => [
+                                    'id' => $configuration['number'],
+                                    'saleable' => $saleable,
+                                    'price' => $formattedOptionPrice
+                                ]
+                            ]
+                        )
+                    );
                 }
             }
 
@@ -366,7 +458,12 @@ class Shopware_Controllers_Frontend_Stylaapi extends Shopware_Controllers_Fronte
         );
 
         $basket = Shopware()->Modules()->Basket();
-        $basket->sAddArticle($orderNumber, $quantity);
+
+        try {
+            $basket->sAddArticle($orderNumber, $quantity);
+        } catch (Exception $e) {
+            $this->throwErr($e->getMessage());
+        }
 
         $this->ajaxCartAction();
     }
